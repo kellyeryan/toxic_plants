ac2a57665a73af3a2260afd438698d38824e9fb1
kellyeryan <kellyeryan@gmail.com>
2019-09-22 10:32:35 -0400

    Fix rubocop offenses

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,18 +8,7 @@ class Cli
   def self.call
     welcome
     find_plants_by_letter
-    puts ""
-    puts "What is the number of the plant you would like to look up?"
-    id = gets.chomp
-    plant = Plant.find_by_id(id)
-    PlantInfoScraper.new(plant)
-    puts ""
-    puts "Additional names:#{plant.additional_common_names}"
-    puts ""
-    puts "Scientific name:#{plant.scientific_name}"
-    puts ""
-    puts "These are the symptoms that occur when the cat eats #{plant.common_name.downcase}."
-    puts plant.clinical_signs
+    find_plants_by_id
   end
 
   def self.welcome
@@ -28,12 +17,25 @@ class Cli
   end
 
   def self.find_plants_by_letter
-    puts "What is the first letter of the plant you'd like to look up?"
+    puts "What is the first letter of the plant you would like to look up?"
     letter = gets.chomp
     PlantsScraper.new(letter).create_plants
     Plant.all.each do |plant|
       puts "#{plant.id}. #{plant.common_name}"
     end
+    puts ""
+  end
+
+  def self.find_plants_by_id
+    puts "What is the number of the plant you would like to look up?"
+    id = gets.chomp
+    plant = Plant.find_by_id(id)
+    PlantInfoScraper.new(plant)
+    puts ""
+    puts "Additional names:#{plant.additional_common_names}\n\n"
+    puts "Scientific name:#{plant.scientific_name}\n\n"
+    puts "These are the symptoms that occur when the cat eats #{plant.common_name.downcase}."
+    puts plant.clinical_signs
   end
 end
 
